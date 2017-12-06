b847a73b59148cf732764d949e6c4d32d4dc9b00
panda404 <13342298293@163.com>
2017-12-06 14:42:22 +0800

    1

--- read_ckpt.py
+++ read_ckpt.py
@@ -8,8 +8,14 @@ weights = []
 names = [ 'Variable_' + str(x) for x in range(54)]
 names[0] = 'Variable'
 reader = tf.train.NewCheckpointReader(weights_file)
-weights.append(reader.get_tensor('Variable'))
+#weights.append(reader.get_tensor('Variable'))
+for name in names:
+    weights.append(reader.get_tensor(name))
 
-layers = ['conv1','max_pool1','conv2','max_pool2',
+layer_names = ['conv1s','max_pool1','conv2','max_pool2',
             'conv3_1','conv3_2','conv3_3','conv3_4','max_pool3',
-            'conv4_1','conv4_2','conv4_3','conv4_4','conv4_5','conv4_6','conv4_7','conv4_8','conv4_9','conv4_10','max_pool4']
+            'conv4_1','conv4_2','conv4_3','conv4_4','conv4_5','conv4_6','conv4_7','conv4_8','conv4_9','conv4_10','max_pool4',
+            'conv5_1','conv5_2','conv5_3','conv5_4','conv5_5','conv5_6s',
+            'conv6_1','conv6_2',
+            'full7','full8','full9']
+print(len(weights))
