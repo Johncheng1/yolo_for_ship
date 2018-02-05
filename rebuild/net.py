6a29b76d23f0533e8c2288e2623fc1a49ce86f09
panda404 <13342298293@163.com>
2018-02-05 23:18:48 +0800

    今天把在线把数据搞定了

--- rebuild/net.py
+++ rebuild/net.py
@@ -188,11 +188,13 @@ class Dataset:
     # type 是随机还是按顺序返回
     def get_placeholder(self, index, num, type):
         pass
+    def pack()
 if __name__ == '__main__':
-    img = cv2.imread('test.jpg')
+    pass
+    ''' img = cv2.imread('test.jpg')
     img = cv2.resize(img,(448,448),interpolation=cv2.INTER_CUBIC)
     inputs = np.zeros((1,448,448,3),dtype='float32')
     inputs[0] = (img/255.0)*2.0-1.0
     net = Net(0, weights_small, layer_names)
     out = net.run(inputs)
-    print(out)
+    print(out) '''
